532e306f2968cb0d0f180782b4ec815a0e184f3f
sospo <sporysh.vladyslav@student.sumdu.edu.ua>
2025-12-03 19:47:45 +0200

    Практична робота 5

--- my-angular-app/src/app/item-list/item-list.ts
+++ my-angular-app/src/app/item-list/item-list.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DiscountItem } from '../shared/models/discount-item.model';
 import { ItemCard } from '../item-card/item-card';
+import { DataService } from '../shared/services/data.service';
 
 @Component({
   selector: 'app-item-list',
@@ -11,39 +12,16 @@ import { ItemCard } from '../item-card/item-card';
   templateUrl: './item-list.html',
   styleUrl: './item-list.css',
 })
-export class ItemList {
+export class ItemList implements OnInit {
 
   searchTerm: string = '';
+  items: DiscountItem[] = [];
 
-  items: DiscountItem[] = [
-    {
-      id: 1,
-      title: 'Знижка на смартфон',
-      store: 'Comfy',
-      oldPrice: 12000,
-      newPrice: 8999,
-      validUntil: '2025-12-01',
-      imageUrl: 'https://th.bing.com/th/id/OIP.sjDmmqF77-usz7TOHHdkywHaFj?w=268&h=201&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-    },
-    {
-      id: 2,
-      title: 'Акція на ноутбук',
-      store: 'Rozetka',
-      oldPrice: 25000,
-      newPrice: 19999,
-      validUntil: '2025-12-10',
-      imageUrl: 'https://i.moyo.ua/img/smart_filters/pp_0EXXzP_image_1694680030.jpg'
-    },
-    {
-      id: 3,
-      title: 'Знижка на кавоварку',
-      store: 'Foxtrot',
-      oldPrice: 8000,
-      newPrice: 3999,
-      validUntil: '2025-11-30',
-      imageUrl: 'https://content.rozetka.com.ua/goods/images/big_tile/473894722.jpg'
-    }
-  ];
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.items = this.dataService.getItems();
+  }
 
   filteredItems() {
     return this.items.filter(item =>
---
+++ my-angular-app/src/app/shared/services/data.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { DiscountItem } from '../models/discount-item.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private items: DiscountItem[] = [
+    {
+      id: 1,
+      title: 'Знижка на смартфон',
+      store: 'Comfy',
+      oldPrice: 12000,
+      newPrice: 8999,
+      validUntil: '2025-12-01',
+      imageUrl: 'https://th.bing.com/th/id/OIP.sjDmmqF77-usz7TOHHdkywHaFj?w=268&h=201&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
+    },
+    {
+      id: 2,
+      title: 'Акція на ноутбук',
+      store: 'Rozetka',
+      oldPrice: 25000,
+      newPrice: 19999,
+      validUntil: '2025-12-10',
+      imageUrl: 'https://i.moyo.ua/img/smart_filters/pp_0EXXzP_image_1694680030.jpg'
+    },
+    {
+      id: 3,
+      title: 'Знижка на кавоварку',
+      store: 'Foxtrot',
+      oldPrice: 8000,
+      newPrice: 3999,
+      validUntil: '2025-11-30',
+      imageUrl: 'https://content.rozetka.com.ua/goods/images/big_tile/473894722.jpg'
+    }
+  ];
+
+  getItems(): DiscountItem[] {
+    return this.items;
+  }
+}
